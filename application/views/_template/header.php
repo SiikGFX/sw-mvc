0387cf1e24b4cd13ff8d20ff61dee17f209888e1
SiikGFX <UniQGameTime@gmail.com>
2018-07-11 17:02:23 +0300

    tweaking jQuery for push optimization

--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -6,7 +6,7 @@
 
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css">
 
-    <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>public/js/main.js"></script>
 </head>
 <body>
